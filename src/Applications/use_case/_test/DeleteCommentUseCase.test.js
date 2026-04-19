ac04bd72ac72651008e37a64d797749095d0daf7
halimalaziz <sanankciez@gmail.com>
2026-04-19 22:54:08 +0700

    update delete

---
+++ src/Applications/use_case/_test/DeleteCommentUseCase.test.js
@@ -0,0 +1,58 @@
+import ThreadRepository from '../../../Domains/threads/ThreadRepository.js';
+import CommentRepository from '../../../Domains/comments/CommentRepository.js';
+import DeleteCommentUseCase from '../DeleteCommentUseCase.js';
+
+describe('DeleteCommentUseCase', () => {
+  /**
+   * Menguji apakah use case mampu mengorkestrasikan langkah demi langkah dengan benar.
+   */
+  it('should orchestrating the delete comment action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      threadId: 'thread-123',
+      commentId: 'comment-123',
+      userId: 'user-123',
+    };
+
+    /** creating dependency of use case */
+    const mockThreadRepository = new ThreadRepository();
+    const mockCommentRepository = new CommentRepository();
+
+    /** mocking needed function */
+    mockThreadRepository.verifyThreadExist = jest
+      .fn(() => Promise.resolve());
+
+    mockCommentRepository.verifyCommentExist = jest
+      .fn(() => Promise.resolve());
+
+    mockCommentRepository.verifyCommentOwner = jest
+      .fn(() => Promise.resolve());
+
+    mockCommentRepository.deleteCommentById = jest
+      .fn(() => Promise.resolve());
+
+    /** creating use case instance */
+    const deleteCommentUseCase = new DeleteCommentUseCase({
+      commentRepository: mockCommentRepository,
+      threadRepository: mockThreadRepository,
+    });
+
+    // Action
+    await deleteCommentUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(mockThreadRepository.verifyThreadExist).toBeCalledWith(
+      useCasePayload.threadId,
+    );
+    expect(mockCommentRepository.verifyCommentExist).toBeCalledWith(
+      useCasePayload.commentId,
+    );
+    expect(mockCommentRepository.verifyCommentOwner).toBeCalledWith({
+      commentId: useCasePayload.commentId,
+      userId: useCasePayload.userId,
+    });
+    expect(mockCommentRepository.deleteCommentById).toBeCalledWith(
+      useCasePayload.commentId,
+    );
+  });
+});
